76b1b59fc2a955cf70717e46e8967e156c0c35b3
ShaunZhou517 <85701185+ShaunZhou517@users.noreply.github.com>
2021-11-13 04:12:42 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+//Initialize the express 'app' object
+let express = require('express');
+let app = express();
+app.use('/', express.static('public'));
+
+//Initialize the actual HTTP server
+let http = require('http');
+let server = http.createServer(app);
+let port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log("Server listening at port: " + port);
+});
+
+//Initialize socket.io
+let io = require('socket.io');
+io = new io.Server(server);
+
+//Listen for individual clients/users to connect
+io.sockets.on('connection', function(socket) {
+    console.log("We have a new client: " + socket.id);
+
+    //Listen for a message named 'data' from this client
+    socket.on('data', function(data) {
+        //Data can be numbers, strings, objects
+        //console.log("Received: 'data' " + data);
+
+        //Send the data to all clients, including this one
+        //Set the name of the message to be 'data'
+        io.sockets.emit('data', data);
+    });
+
+    //Listen for a message named 'pressed' from this client
+    socket.on('pressed', function(data) {
+        //Data can be numbers, strings, objects
+        //console.log("Pressed: 'data' " + data);
+
+        //Send the data to all clients, including this one
+        //Set the name of the message to be 'data'
+        io.sockets.emit('pressed', data);
+    });
+
+    //Listen for this client to disconnect
+    socket.on('disconnect', function() {
+        console.log("A client has disconnected: " + socket.id);
+    });
+});
